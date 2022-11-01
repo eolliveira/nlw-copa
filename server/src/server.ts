471ecbd6311d75272011e76ae3f33caaede66409
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-31 23:12:53 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import Fastify from "fastify";
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  await fastify.get("/pools/count", () => {
+    return { count: 1000 };
+  });
+
+  await fastify.listen({ port: 3000 });
+}
+
+//função start projeto
+bootstrap();
