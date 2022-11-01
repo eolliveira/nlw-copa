5129beead86a9c62034bb9da143d76e867fe1a25
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-01 00:19:51 -0300

    teste

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,27 @@
 //equivalente o express
+import { Prisma, PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+
+//log de sql
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  await fastify.get("/pools/count", () => {
-    return { count: 1000 };
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        title: {
+          startsWith: 'B'
+        }
+      }
+    })
+
+    return { pools } 
   });
 
   await fastify.listen({ port: 3000 });
