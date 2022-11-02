3e2de81f7f46d6d17f6a7bc2a5c964a22945dc20
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-02 17:10:40 -0300

    Customizando tema do Native Base

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider } from "native-base";
+import { Center, NativeBaseProvider, Text } from "native-base";
+
+import { THEME } from './src/styles/theme'
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Hello World</Text>
+    <NativeBaseProvider theme={ THEME }>
+      <Center flex={1} bgColor="green.500">
+        <Text color="amber.400" >Hello World</Text>
         <StatusBar style="auto" />
-      </View>
+      </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
