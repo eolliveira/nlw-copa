05cd9899ea94227c16f5a5cb8b926005bb9d5a6a
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-01 22:00:29 -0300

    liberando cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 //equivalente o express
 import { Prisma, PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import cors from '@fastify/cors'
 
 //log de sql
 const prisma = new PrismaClient({
@@ -12,13 +13,19 @@ async function bootstrap() {
     logger: true,
   });
 
+
+  //define o cors da aplicação como publico 
+  await fastify.register(cors , {
+    origin: true
+  })
+
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count(); 
 
     return { count } 
   });
 
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({ port: 3000, host: '0.0.0.0' });
 }
 
 //função start projeto
