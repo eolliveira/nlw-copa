c4ba3d04c943cf4386aeb2b9da147f3b894e36c0
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-04 00:18:41 -0300

    Seed do banco de dados

---
+++ server/prisma/seed.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Erick Oliveira",
+      email: 'jhon2@gmail.com',
+      avatarUrl: "https://github.com/eolliveira.png",
+    },
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Bolão da Empresa",
+      code: "BOLOA123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-09-04T02:27:29.848Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-10-04T02:27:29.848Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 3,
+          SecondTeamPoints: 0,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main()
