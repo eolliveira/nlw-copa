c74a55f05fde9b482e36c5602333ee383e050827
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-06 11:02:33 -0300

    Finalizando tela home

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,59 @@
-interface HomeProps {
-  count: number
-}
+import Image from "next/image";
+import AppNlwCopaPreview from "../assets/app-nlw-copa-preview.png";
+import LogoApp from "../assets/logo.svg";
+import UsersAvatars from "../assets/users-avatar-example.png";
+import IconCheck from "../assets/icon-check.svg";
 
-export default function Home(props: HomeProps) {
-  return <h1 className=" text-lg font-bold text-violet-500" >{`Contagem: ${props.count}`}</h1>;
+
+export default function Home() {
+  return (
+    <div className="max-w-[1124px] mx-auto grid grid-cols-2 gap-28 h-screen items-center " >
+      <main>
+        <Image quality={100} src={LogoApp} alt="logo" />
+ 
+        <h1 className="mt-14 text-white-100 text-5xl font-bold leading-tight">Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div className="mt-10 flex items-center gap-2 text-white-100 text-xl ">
+          <Image quality={100} src={UsersAvatars} alt={"imagem usuários"} />
+          <p>
+            <span className="text-green-500" >+12.592</span> pessoas ja estão usando
+          </p>
+        </div>
+
+        <form className="mt-10 flex gap-1"> 
+          <input className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm" type="text" required placeholder="Qual o nome do seu bolão?" />
+          <button className="bg-yellow-500 px-6 py-4 rounded uppercase text-sm font-bold text-gray-900 hover:bg-yellow-700" type="submit">Criar um bolão</button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você recebera um código unico que podera usar
+          para convidar outras pessoas
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6"> 
+            <Image quality={100} src={IconCheck} alt={"icone check"}></Image>
+            <span className="flex flex-col">
+              <strong>+2.034</strong>
+              <p>Bolões criados</p>
+            </span>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
+            <Image quality={100} src={IconCheck} alt={"icone check" }></Image>
+            <span className="flex flex-col">
+              <strong>+2.034</strong>
+              <p>Palpites enviados</p>
+            </span>
+          </div>
+        </div>
+      </main>
+
+      <Image src={AppNlwCopaPreview} alt={"Banner"} quality={100} />
+    </div>
+  );
 }
 
 //ServerSiteRendering = renderização e construção visual no node
@@ -19,4 +69,3 @@ export const getServerSideProps = async () => {
     },
   };
 };
-
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,11 +9,29 @@ module.exports = {
       },
     },
 
+    backgroundImage :{
+      app: 'url(/app-bg.png)'
+    },
+
     colors: {
       gray: {
+        100: '#E1E1E6',
+        300: '#8D8D99',
+        600: '#323238',
+        800: '#202024',
         900: "#121214",
       },
-    },
+      white: {
+        100: '#FFFFFF'   
+      },
+      green: {
+        500: '#129E57'   
+      },
+      yellow: {
+        500: '#F7DD43',
+        700: '#D1CC43'
+      }
+    }      
   },
   plugins: [],
 };
