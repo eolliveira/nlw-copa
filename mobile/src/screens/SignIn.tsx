92fc8fdec6f8196dcb77485ea2be6760e9c756fc
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-11-06 16:55:49 -0300

    Tipando SVG

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,11 @@
-import { Center, Text } from "native-base";
+import { Center } from "native-base";
 
 import Logo from '../assets/logo.svg'
 
 export const SingnIn = () => {
   return (
     <Center flex={1} bg='gray.900'>
-       <Logo />
+       <Logo width={212} height={40} />
     </Center>
   );
 };
